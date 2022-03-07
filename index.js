37882e496e4252d8b02cc61ad1ad0deb661e3b66
Hazvi Nurafgan <hazvinurafgan@gmail.com>
2022-03-07 19:56:00 +0700

    dev some import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import Router from './src/Router'
-import { AppRegistry, View, Text, Platform, Dimensions } from 'react-native'
+import { AppRegistry } from 'react-native'
 import { name } from './app.json'
 import { NavigationContainer } from '@react-navigation/native'
 
@@ -9,15 +9,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import { store, pStore } from './src/redux/store'
 
-import FastImage from 'react-native-fast-image'
-
 import Loading from './src/commons/components/Loading'
-
-const vehicleRentalLoading = require('./src/assets/gif/vehicleRentalLoading.gif')
-
-const deviceWidth = Dimensions.get('window').width
-const deviceHeight = Platform.OS === 'android' ? Dimensions.get('window').height :
-    require('react-native-extra-dimensions-android').get('REAL_WINDOW_HEIGHT')
 
 const { registerComponent } = AppRegistry
 
